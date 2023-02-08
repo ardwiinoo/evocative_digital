16b6e68ccc869ec421ca52eea8200d1dbf9c6a33
mulyant20 <mulyan.t20@gmail.com>
2023-02-08 23:50:09 +0700

    feat: navbar-and-dropdown

---
+++ src/assets/js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // tambahkan padding top untuk menampilkan konten di belakang navbar
+  navbar_height = document.querySelector('.navbar').offsetHeight
+  document.body.style.paddingTop = navbar_height + 'px'
+})
+
+// AOS init
+AOS.init({
+  once: true,
+})
+
+// const accordions = document.querySelectorAll('.accordion-head')
+
+// accordions.forEach(accordion => {
+//   accordion.addEventListener('click', () => {
+//     accordion.classList.toggle('active')
+//   })
+// })
+
+const dropdownContainers = document.querySelectorAll('.dropdown-container')
+
+dropdownContainers.forEach((dropdown, idx) => {
+  dropdown.addEventListener('click', () => {
+    hideAllDropdown()
+    dropdown.classList.add('active')
+  })
+  
+  dropdown.addEventListener('blur', () => {
+    hideAllDropdown()
+  })
+})
+
+function hideAllDropdown() {
+  dropdownContainers.forEach((drop) => {
+    drop.classList.remove('active')
+  })
+}
